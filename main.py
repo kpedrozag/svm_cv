fe277de120b534cfa6ab4e49984affd80130a529
Kevin pedroza <kevin.andres01@hotmail.com>
2018-10-28 15:50:33 -0500

    right standarization

--- main.py
+++ main.py
@@ -14,28 +14,44 @@ from sklearn.svm import SVC
 import sklearn.metrics as mtr
 import numpy as np
 
+# Dataset is imported
 X, y = load_breast_cancer(return_X_y=True)
 
-scaler = StandardScaler()
-X= scaler.fit_transform(X)
-
-
+# data is splitted, stratifying
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.30,
                                                     random_state=0, 
                                                     stratify=y)
 
+# the scaler object is created
+# the mean and std_var is computed when 
+# the fit() method is called
+scaler = StandardScaler().fit(X_train)
+
+# standarization of the X_train set is performed
+X_train = scaler.transform(X_train)
+
+# Several values for the penalty parameter are done
 c = [10**i for i in range(-10,11)]
 
-
+# It is computed some models of SVM with the linear kernel
+# where the C parameter is changed
 svm_list = [SVC(C=i, kernel='linear', 
                 random_state=0).fit(X_train, y_train) for i in c]
 
+# score of each svm model
 score = [cross_val_score(st, X_train, y_train, cv=30).mean() for st in svm_list]
 
+# index of the model with the highest score
 pos_max_score = np.argmax(score)
 
+# Data training with the best svm model
 svm_valid = SVC(C=c[pos_max_score], kernel='linear',
                 random_state=0).fit(X_train, y_train)
+
+
+s_vectors = svm_valid.support_vectors_
+
+X_test = scaler.transform(X_test)
 
 model = svm_valid.predict(X_test)
 
